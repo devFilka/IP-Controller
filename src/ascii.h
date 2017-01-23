617e5168454309c2cf32f5b7a04f1c5ec5969500
devFilka <filka.dev@gmail.com>
2017-01-23 17:05:51 +0300

    Add ascii control codes

---
+++ src/ascii.h
@@ -0,0 +1,37 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+#define NUL  0 /* Null */
+#define SOH  1 /* Start of heading */
+#define STX  2 /* Start of text */
+#define ETX  3 /* End of text */
+#define EOT  4 /* End of transmission */
+#define ENQ  5 /* Enquiry */
+#define ACK  6 /* Acknowledge */
+#define BEL  7 /* Bell */
+#define BS   8 /* Backspace */
+#define HT   9 /* Horizontal tab */
+#define LF  10 /* Line feed */
+#define VT  11 /* Vertical tab */
+#define FF  12 /* Form feed */
+#define CR  13 /* Carriage return */
+#define SO  14 /* Shift out */
+#define SI  15 /* Shift in */
+#define DLE 16 /* Data line escape */
+#define DC1 17 /* Device control 1 */
+#define DC2 18 /* Device control 2 */
+#define DC3 19 /* Device control 3 */
+#define DC4 20 /* Device control 4 */
+#define NAK 21 /* Negative acknowledge */
+#define SYN 22 /* Synchronous idle */
+#define ETB 23 /* End transmission block */
+#define CAN 24 /* Cancel */
+#define EM  25 /* End of medium */
+#define SUB 26 /* Substitute */
+#define ESC 27 /* Escape */
+#define FS  28 /* File separator */
+#define GS  29 /* Group separator */
+#define RS  30 /* Record separator */
+#define US  31 /* Unit separator */
+
+#endif
