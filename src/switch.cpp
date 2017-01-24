d739206e757e88303cc77b281f63d23fcfbb5298
devFilka <filka.dev@gmail.com>
2017-01-24 15:22:49 +0300

    Add switch on/off functions

---
+++ src/switch.cpp
@@ -0,0 +1,59 @@
+#include "time.h"
+#include "switch.h"
+
+static bool is_relay_on;
+
+void switch_realy_on() {
+  pinMode( SWITCH_RELAY, OUTPUT );
+  digitalWrite( SWITCH_RELAY, HIGH );
+  is_relay_on = true;
+}
+void switch_realy_off() {
+  pinMode( SWITCH_RELAY, OUTPUT );
+  digitalWrite( SWITCH_RELAY, LOW );
+  is_relay_on = false;
+}
+bool switch_reset(uint32_t reset_delay){
+  static uint32_t time = millis();
+  if( !is_relay_on ) {
+    if( is_time_end( reset_delay, time, millis()) ) {
+      switch_realy_on();
+      time = millis();
+      return true;
+    }
+    else {
+      switch_realy_off();
+      return false;
+    }
+  }
+  else {
+    if( is_time_end( reset_delay, time, millis()) ) {
+      switch_realy_on();
+      time = millis();
+      return true;
+    }
+    else {
+      switch_realy_off();
+      return false;
+    }
+  }
+}
+
+uint8_t get_switch_status(bool is_need_on){
+  static uint8_t switch_state = SWITCH_START;
+  switch (switch_state) {
+    case SWITCH_START:
+    case SWITCH_ON:
+    case SWITCH_OFF:
+      if( is_need_on ) {
+        switch_state = SWITCH_ON;
+      }
+      else {
+        switch_state = SWITCH_OFF;
+      }
+    default:
+      switch_state = SWITCH_HZ;
+      break;
+  }
+  return switch_state;
+}
---
+++ src/switch.h
@@ -0,0 +1,27 @@
+#ifndef SWITCH_H
+#define SWITCH_H
+
+/* Arduino header */
+#include "Arduino.h"
+
+/* Switch defines */
+#define SWITCH_RELAY 49
+//#define SWITCH_LED 43
+
+/* Switch constants */
+#define SWITCH_RESET_DELAY 3000
+
+/* Switch states */
+#define SWITCH_START 0
+#define SWITCH_ON 1
+#define SWITCH_OFF 2
+#define SWITCH_HZ 100
+
+/* Switch functions */
+void switch_realy_on();
+void switch_realy_off();
+bool switch_reset(uint32_t reset_delay);
+
+uint8_t get_switch_status(bool is_need_on);
+
+#endif
