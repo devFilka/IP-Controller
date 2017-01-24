efdada68a94cd57dba4efc77867e1babcb92f1e2
devFilka <filka.dev@gmail.com>
2017-01-24 16:52:38 +0300

    template

---
+++ src/power.h
@@ -0,0 +1,15 @@
+#ifndef POWER_H
+#define POWER_H
+
+/* Arduino header */
+#include "Arduino.h"
+
+/* Power defines */
+
+/* Power constants */
+
+/* Power states */
+
+/* Power functions */
+
+#endif
