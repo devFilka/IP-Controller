2e8f08b495b0f0411f4e459e782733af45a4a931
devFilka <filka.dev@gmail.com>
2017-01-31 16:58:34 +0300

    add power on/off functions

--- src/power.cpp
+++ src/power.cpp
@@ -1,11 +1,21 @@
 #include "power.h"
 
 /* battery functions */
-void off_baterry(){
+void battery_off(){
   digitalWrite(BATTERY_POWER_PIN, LOW);
+  pinMode( BATTERY_POWER_PIN, OUTPUT );
 }
-void on_baterry(){
+void battery_on(){
   digitalWrite(BATTERY_POWER_PIN, HIGH);
+  pinMode( BATTERY_POWER_PIN, OUTPUT );
+}
+void power_off(){
+  digitalWrite(POWER_ALL_PIN, LOW);
+  pinMode( POWER_ALL_PIN, OUTPUT );
+}
+void power_on(){
+  digitalWrite(POWER_ALL_PIN, HIGH);
+  pinMode( POWER_ALL_PIN, OUTPUT );
 }
 
 /*  */
--- src/power.h
+++ src/power.h
@@ -6,12 +6,16 @@
 
 /* Power defines */
 #define BATTERY_POWER_PIN  46
-
+#define POWER_ALL_PIN  22
 
 /* Power constants */
 
 /* Power states */
 
 /* Power functions */
+void battery_off();
+void battery_on();
+void power_off();
+void power_on();
 
 #endif
